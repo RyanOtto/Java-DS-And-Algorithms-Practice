b71ba7b91d778482611723c6130d025b0883c8ff
Ryan Otto <rotto@drew.edu>
2017-04-26 22:53:05 -0400

    Added mergeSort method to Main class

--- src/Main.java
+++ src/Main.java
@@ -79,17 +79,25 @@ public class Main {
 //	       System.out.print(me.getKey() + ": ");
 //	       System.out.println(me.getValue());
 		
+		int[] myArray = {2,3,1,6,5,7,8,1,8,10,20,100,50,25,34,46,58};
+//		System.out.println("Array before sorting: " + Arrays.toString(myArray));
+//		quickSort(myArray, 0, myArray.length-1);
+//		System.out.println("Array after sorting: " + Arrays.toString(myArray));
+		
 		System.out.println("Array before sorting: " + Arrays.toString(myArray));
-		quickSort(myArray, 0, myArray.length-1);
+//		quickSort(myArray, 0, myArray.length-1);
+		mergeSort(myArray, 0, myArray.length-1);
 		System.out.println("Array after sorting: " + Arrays.toString(myArray));
 }
+
+	
 	
 	
 		public static void quickSort(int[] array, int left, int right) {
 			if (array == null || array.length == 0) return;
 			if (left >= right) return;
 	 
-			int middle = left + (right - left) / 2;
+			int middle = (left + right) / 2;
 			int i = left, j = right;
 			
 			while (i < j) {
@@ -108,4 +116,53 @@ public class Main {
 			if (left < j) quickSort(array, left, j);
 			if (right > i) quickSort(array, i, right);
 		}
-	}
+		
+		
+		
+		
+	    private static void mergeSort(int[] array, int left, int right) {
+	        if(left < right) { //If there's more than 1 element in the list
+	            int center = (left + right) / 2; //Center point for list splitting
+	            mergeSort(array, left, center); //Sort from beginning to center
+	            mergeSort(array, center+1, right); //Sort from center+1 to end
+	            merge(array, left, center+1, right); //Merge the two resulting arrays
+	        }
+	    }
+	    
+	    private static void merge(int[] array, int leftIndex, int rightIndex, int rightEnd) {
+
+	        int leftArrayEnd = rightIndex - 1; //Left array ends with the beginning of the right array
+	        int numElements = rightEnd - leftIndex + 1; //Number of elements equal to end-beginning+1
+	        int[] resultArray = new int[numElements];
+	        int resultArrayIndex = 0;
+
+	        while (leftIndex <= leftArrayEnd && rightIndex <= rightEnd) { //While both lists have elements to go through
+	            if (array[leftIndex] <= array[rightIndex]) { //If current left element is lower
+	                resultArray[resultArrayIndex] = array[leftIndex]; //Add it to the result array
+	                resultArrayIndex++;
+	                leftIndex++; //Deal with the next left list element (if there is one)
+	            } else{ //If current right element is lower
+	            	resultArray[resultArrayIndex] = array[rightIndex]; //Add it to the result array
+	            	resultArrayIndex++;
+	            	rightIndex++; //Deal with the next right list element (if there is one)
+	            }
+	        }
+	        
+	        //Add leftover left list elements to result array
+	        while (leftIndex <= leftArrayEnd) { 
+	            resultArray[resultArrayIndex] = array[leftIndex]; 
+	            resultArrayIndex++;
+	            leftIndex++;
+	        }
+
+	        //Add leftover right list elements to result array
+	        while (rightIndex <= rightEnd) { 
+	            resultArray[resultArrayIndex] = array[rightIndex];
+	            resultArrayIndex++;
+	            rightIndex++;
+	        }
+
+	        //Move backwards to the first position in the original array, setting each position to the result array value in the index
+	        for (int i = numElements-1; i >= 0; i--, rightEnd--) array[rightEnd] = resultArray[i];
+    }
+}
